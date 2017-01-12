0a7d3ac3d3a07de056240dfe4657ac95d9d2f955
Philip Caleja <pcaleja@gmail.com>
2017-01-12 01:32:12 -0500

    improve gigasecond.js

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,8 +3,7 @@ var Gigasecond = function(date) {
 };
 
 Gigasecond.prototype.date = function() {
-  const ms = Date.parse(this.inputDate) + 1e12;
-  return new Date(ms);
+  return new Date(this.inputDate.getTime() + 1e12);
 }
 
 module.exports = Gigasecond;
