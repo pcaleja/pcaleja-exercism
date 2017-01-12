79bff13ffdb3c862cdf9f56ad102ca457cd451cd
Philip Caleja <pcaleja@gmail.com>
2017-01-12 03:24:26 -0500

    improve leap.js

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,15 @@
+'use strict';
+
 var Year = function(input) {
-  this.year = input;
+  this.isDivisibleBy400 = input % 400 === 0;
+  this.isDivisibleBy4 = input % 4 === 0;
+  this.isNotDivisibleBy100 = input % 100 !== 0;
 };
 
 Year.prototype.isLeap = function() {
-  return (this.year % 400 === 0) || (this.year % 4 === 0 && this.year % 100 !== 0)
-}
+  return this.isDivisibleBy400 ||
+         this.isDivisibleBy4 &&
+         this.isNotDivisibleBy100;
+};
 
 module.exports = Year;
