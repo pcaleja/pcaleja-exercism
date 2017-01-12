1c33f87a0e162f799325efe7672e6226289662e3
Philip Caleja <pcaleja@gmail.com>
2017-01-12 03:54:13 -0500

    improve isogram.js

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,20 +1,24 @@
+'use strict';
+
 var Isogram = function(string) {
   this.wordArray = string
     .toLowerCase()
     .split('')
     .sort()
-    .filter(n => n.match(/(?!\s|\-)\W|[a-z]/g));
-}
+    .filter(lettersOnly);
+
+  function lettersOnly(char) {
+    return char.match(/(?!\s|\-)\W|[a-z]/g);
+  }
+};
 
 Isogram.prototype.isIsogram = function() {
-  console.log(this.wordArray);
-  var value = true;
   for (var i = 0; i < this.wordArray.length - 1; i++) {
     if (this.wordArray[i] === this.wordArray[i + 1]) {
-      value = false;
+      return false;
     }
   }
-  return value;
-}
+  return true;
+};
 
 module.exports = Isogram;
