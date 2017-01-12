18cb03132e9a739f586522193d036eb2370e408d
Philip Caleja <pcaleja@gmail.com>
2017-01-11 23:53:13 -0500

    improve leap.js

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,20 +3,7 @@ var Year = function(input) {
 };
 
 Year.prototype.isLeap = function() {
-  var leapBoolean;
-
-  switch (true) {
-    case (this.year % 400 === 0):
-      leapBoolean = true;
-      break;
-    case (this.year % 4 === 0 && this.year % 100 !== 0):
-      leapBoolean = true;
-      break;
-    default:
-      leapBoolean = false;
-  }
-
-  return leapBoolean
+  return (this.year % 400 === 0) || (this.year % 4 === 0 && this.year % 100 !== 0)
 }
 
 module.exports = Year;
